223d5224850bf147fe8723d85dcdde4b032e6bed
simon1003 <23188138+simon1003@users.noreply.github.com>
2019-05-07 08:27:45 +0800

    第二次提交

---
+++ DataAnalysis/DataAnalysis.Framework.Common/Json/JsonResult.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAnalysis.Framework.Common.Json
+{
+    [DataContract]
+    public class JsonResult
+    {
+        private bool _resultFlag = true;
+        private object _dataSource;
+
+        public JsonResult(object data)
+        {
+            ResultMessage = "1";
+            _dataSource = data;
+            _resultFlag = true;
+        }
+
+        public JsonResult()
+        {
+
+        }
+
+        [DataMember]
+        public bool ResultFlag
+        {
+            get { return _resultFlag; }
+            set { _resultFlag = value; }
+        }
+
+        [DataMember]
+        public string ResultMessage { get; set; }
+
+        [DataMember]
+        public object DataSource
+        {
+            get { return _dataSource; }
+            set { _dataSource = value; }
+        }
+    }
+}
---
+++ DataAnalysis/DataAnalysis/user_info.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DataAnalysis.Framework.Common.Json;
+namespace DataAnalysis
+{
+    public partial class user_info : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        [WebMethod]
+        public static JsonResult GetUserState()
+        {
+            JsonResult jr = new JsonResult();
+            jr.ResultMessage = "success";
+            return jr;
+        }
+
+
+    }
+}
